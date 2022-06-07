f3ae5b6f12c1955c3b8b9db71b71df814451a180
guidrymwg <guidry@utk.edu>
2022-06-07 16:10:50 -0400

    Continue cleaning up. This version reproduces previous versions.

--- explicitMatrix/explicitMatrix.cpp
+++ explicitMatrix/explicitMatrix.cpp
@@ -310,7 +310,7 @@ double dt_trial[plotSteps];            // Trial dt at plotstep
 int dtMode;                            // Dual dt stage (0=full, 1=1st half, 2=2nd half)
 double XcorrFac;                       // Equil normalization factor for timestep
 
-double massTol = 1e-5;//5e-3;                 // Timestep tolerance parameter (1.0e-7)
+double massTol = 5e-3; //1e-5                // Timestep tolerance parameter (1.0e-7)
 double downbumper = 0.7;               // Asy dt decrease factor
 double sf = 1e25;                      // dt_FE = sf/fastest rate
 int maxit = 20;                        // Max asy dt iterations
@@ -319,7 +319,7 @@ int totalIterations;                   // Total number of iterations
 double Error_Observed;                 // Observed integration error
 double Error_Desired;                  // Desired integration error
 double E_R;                            // Ratio actual to desired error
-double EpsA = 1e-5;//5e-3;                    // Absolute error tolerance
+double EpsA = 5e-3;  // 1e-5                 // Absolute error tolerance
 double EpsR = 2.0e-4;                  // Relative error tolerance (not presently used)
 
 // Time to begin trying to impose partial equilibrium if doPE=true. Hardwired but 
@@ -332,7 +332,7 @@ double EpsR = 2.0e-4;                  // Relative error tolerance (not presentl
 // On the other hand, the check should not be too costly.
 
 double equilibrateTime = 1e-12;    // Time to begin checking for PE
-double equiTol = 0.005;             // Tolerance for checking whether Ys in RG in equil
+double equiTol = 0.01;           // Tolerance for checking whether Ys in RG in equil
 
 double deviousMax = 0.5;      // Max allowed deviation from equil k ratio in timestep
 double deviousMin = 0.1;      // Min allowed deviation from equil k ratio in timestep
