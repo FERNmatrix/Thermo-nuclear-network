4f235ce9111a9c43683ee2028d6ca4ee5ff300e4
guidrymwg <guidry@utk.edu>
2022-12-14 14:07:02 -0500

    Fast alpha T9=7 rho=1e8 Asy+PE

--- explicitMatrix/explicitMatrix.cpp
+++ explicitMatrix/explicitMatrix.cpp
@@ -175,7 +175,7 @@ void indexCNOCycle(void);
 #define ISOTOPES 16                   // Max isotopes in network (e.g. 16 for alpha network)
 #define SIZE 48                      // Max number of reactions (e.g. 48 for alpha network)
 
-#define plotSteps 100                 // Number of plot output steps
+#define plotSteps 200                 // Number of plot output steps
 #define LABELSIZE 35                  // Max size of reaction string a+b>c in characters
 #define PF 24                         // Number entries partition function table for isotopes
 #define THIRD 0.333333333333333
